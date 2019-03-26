a91074cc7b34ca102a98231b959d6149f5a7b235
mp007 <praka027@umn.edu>
2019-03-26 21:26:00 +0100

    Refactor

--- src/main/java/de/csbd/learnathon/command/Utils.java
+++ src/main/java/de/csbd/learnathon/command/Utils.java
@@ -5,6 +5,8 @@ import java.util.Enumeration;
 import javax.swing.AbstractButton;
 import javax.swing.ButtonGroup;
 
+import net.imglib2.realtransform.AffineTransform3D;
+
 public class Utils {
 
 	public static String getSelectedButtonText( ButtonGroup buttonGroup ) {
@@ -16,5 +18,15 @@ public class Utils {
 
 		return null;
 	}
+
+	public static double extractScale( final AffineTransform3D t, final int axis ) { //TODO Move this method to Utils
+		double sqSum = 0;
+		final int c = axis;
+		for ( int r = 0; r < 4; ++r ) {
+			final double x = t.get( r, c );
+			sqSum += x * x;
+		}
+		return Math.sqrt( sqSum );
 	}
 
+}
