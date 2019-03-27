6fb4bea32e716b9609a02257c9918da74f8a8bd1
mp007 <praka027@umn.edu>
2019-03-28 00:08:21 +0100

    Refactored FlowOverlay

--- src/main/java/de/csbd/learnathon/command/FlowOverlay.java
+++ src/main/java/de/csbd/learnathon/command/FlowOverlay.java
@@ -7,7 +7,6 @@ import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 import bdv.util.BdvOverlay;
-import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.type.numeric.real.DoubleType;
@@ -15,11 +14,14 @@ import net.imglib2.type.numeric.real.DoubleType;
 public class FlowOverlay extends BdvOverlay {
 	
 	private RandomAccessibleInterval< DoubleType > flowData;
-	private ArrayList< FlowVector > sparseFlow;
+
+	private ArrayList< FlowVector > handPickedSparseFlowVectors;
 
 	private PunctaPickerView view;
 	
 	private boolean visible = false;
+
+	private ArrayList< FlowVector > spacedFlowVectors;
 
 	public FlowOverlay( PunctaPickerView view ) {
 		this.view = view;
@@ -29,12 +31,20 @@ public class FlowOverlay extends BdvOverlay {
 		this.visible = visible;
 	}
 
-	public void paintDenseFlow( RandomAccessibleInterval< DoubleType > flowData ) {
+	public void setDenseFlow( RandomAccessibleInterval< DoubleType > flowData ) {
 		this.flowData = flowData;
 	}
 
-	public void paintSparseFlow(ArrayList< FlowVector > sparseFlow) {
-		this.sparseFlow = sparseFlow;
+	public void setHandPickedSparseFlow(ArrayList< FlowVector > sparseFlow) {
+		this.handPickedSparseFlowVectors = sparseFlow;
+	}
+
+	public void setSpacedFlow( ArrayList< FlowVector > spacedFlow ) {
+		this.spacedFlowVectors = spacedFlow;
+	}
+
+	public void requestRepaint() {
+		view.getBdv().getViewerPanel().requestRepaint();
 	}
 
 	@Override
@@ -42,15 +52,15 @@ public class FlowOverlay extends BdvOverlay {
 		int t = info.getTimePointIndex();
 
 		if ( visible ) {
-			prepareSparseVectors( g, t );
-			prepareDenseVectors( g, t );
+			drawSparseHandPickedFlow( g, t );
+			drawSpacedFlow( g, t );
 		}
 
 	}
 
-	private void prepareSparseVectors( Graphics2D g, int t ) {
+	private void drawSparseHandPickedFlow( Graphics2D g, int t ) { //Can be simplified to only draw but stays here
 		ArrayList< FlowVector > sparseFlowVectors = new ArrayList<>();
-		for ( FlowVector flowVector : sparseFlow ) {
+		for ( FlowVector flowVector : handPickedSparseFlowVectors ) {
 			if ( flowVector.getT() == t )
 				sparseFlowVectors.add( flowVector );
 		}
@@ -59,41 +69,15 @@ public class FlowOverlay extends BdvOverlay {
 		}
 	}
 
-	private void prepareDenseVectors( Graphics2D g, int t ) {
-		final long sizeX = flowData.dimension( 0 );
-		final long sizeY = flowData.dimension( 1 );
-
-		if ( t < flowData.dimension( 2 ) / 2 ) {
-			int spacing = 10; // spacing between pixels for flow display
-			spacing = Math.max( spacing, ( int ) Math.max( sizeX, sizeY ) / 25 ); // but if large image only 25 vecs along longer side
-
-			int startx = ( int ) ( sizeX % spacing ) / 2;
-			startx = ( startx == 0 ) ? spacing / 2 : startx;
-			int starty = ( int ) ( sizeY % spacing ) / 2;
-			starty = ( starty == 0 ) ? spacing / 2 : starty;
-
-			for ( int x = startx; x < sizeX; x += spacing ) {
-				for ( int y = starty; y < sizeY; y += spacing ) {
-
-					FlowVector flowVec = getFlowVector( flowData, x, y, t );
-					drawVector( g, x, y, flowVec.getU(), flowVec.getV(), Color.RED );
-				}
+	private void drawSpacedFlow( Graphics2D g, int t ) {
+		for ( FlowVector flowVec : spacedFlowVectors ) {
+			if ( flowVec.getT() == t ) {
+				float x = flowVec.getX();
+				float y = flowVec.getY();
+				Color color = flowVec.isSelected() ? Color.LIGHT_GRAY : Color.RED;
+				drawVector( g, ( int ) x, ( int ) y, flowVec.getU(), flowVec.getV(), color );
 			}
 		}
-	}
-
-	private FlowVector getFlowVector( RandomAccessibleInterval< DoubleType > f, int x, int y, int t ) {
-		RandomAccess< DoubleType > ra = f.randomAccess();
-		ra.setPosition( x, 0 );
-		ra.setPosition( y, 1 );
-		ra.setPosition( 2 * t, 2 );
-		Double u = ra.get().getRealDouble();
-		ra.setPosition( x, 0 );
-		ra.setPosition( y, 1 );
-		ra.setPosition( 2 * t + 1, 2 );
-		Double v = ra.get().getRealDouble();
-		FlowVector flowVector = new FlowVector( x, y, t, u, v );
-		return flowVector;
 	}
 
 	private void drawVector( final Graphics2D g, int x, int y, double u, double v, Color c ) {
