ad99090954f4036675860f653e939abe3f23c10b
mp007 <praka027@umn.edu>
2019-04-04 21:50:23 +0200

    Computes dense flow vectors

--- src/main/java/de/csbd/learnathon/command/FlowVectorsCollection.java
+++ src/main/java/de/csbd/learnathon/command/FlowVectorsCollection.java
@@ -2,13 +2,17 @@ package de.csbd.learnathon.command;
 
 import java.util.ArrayList;
 
+import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
 
 public class FlowVectorsCollection {
 
 	private FlowVector onlySelectedFlowVector; //The only flow vector which is set as selected at a given time, can be null as well
 	private ArrayList< FlowVector > sparseHandPickedFlow;
+	private ArrayList< FlowVector > autoFeatureFlow;
 	private RandomAccessibleInterval< DoubleType > denseFlow;
 	private ArrayList< FlowVector > spacedFlow;
 	private ArrayList< FlowVector > editedFlow = new ArrayList<>();
@@ -32,6 +36,10 @@ public class FlowVectorsCollection {
 		this.sparseHandPickedFlow = sparseHandPickedFlow;
 	}
 
+	public void setAutoFeatureFlow( ArrayList< FlowVector > autoFeatureFlow ) {
+		this.autoFeatureFlow = autoFeatureFlow;
+	}
+
 	public void setDenseFlow( RandomAccessibleInterval< DoubleType > denseFlow ) {
 		this.denseFlow = denseFlow;
 	}
@@ -42,6 +50,10 @@ public class FlowVectorsCollection {
 
 	public ArrayList< FlowVector > getSparsehandPickedFlowVectors() {
 		return sparseHandPickedFlow;
+	}
+
+	public ArrayList< FlowVector > getAutofeatureFlowVectors() {
+		return autoFeatureFlow;
 	}
 
 	public RandomAccessibleInterval< DoubleType > getDenseFlow() {
@@ -71,5 +83,28 @@ public class FlowVectorsCollection {
 		}
 		else
 			return null;
+	}
+
+	public ArrayList< FlowVector > getDenseFlowVectors() { //Only experimental, needs deletion later
+		ArrayList< FlowVector > dfvec = new ArrayList<>();
+		RandomAccessibleInterval< DoubleType > df = getDenseFlow();
+		int count = 0;
+		for ( int slice = 0; slice < df.dimension( 2 ); slice = slice + 2 ) {
+			IntervalView< DoubleType > u = Views.hyperSlice( df, 2, slice );
+			IntervalView< DoubleType > v = Views.hyperSlice( df, 2, slice+1 );
+			RandomAccess< DoubleType > ura = u.randomAccess();
+			RandomAccess< DoubleType > vra = v.randomAccess();
+			for(int x =0; x < df.dimension( 0 ); x++) {
+				for(int y = 0; y < df.dimension( 1 ); y++) {
+					ura.setPosition( x, 0 );
+					ura.setPosition( y, 1 );
+					vra.setPosition( x, 0 );
+					vra.setPosition( y, 1 );
+					dfvec.add( new FlowVector( x, y, count, ura.get().getRealDouble(), vra.get().getRealDouble() ) );
+				}
+			}
+			count = count + 1;
+		}
+		return dfvec;
 	}
 }
