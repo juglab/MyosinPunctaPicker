0655c7ac22bd7525389def83d5db075e6aeebf0d
mp007 <praka027@umn.edu>
2020-03-24 16:21:40 +0100

    Fix typo in name

--- src/main/java/de/csbd/learnathon/command/PunctaPickerView.java
+++ src/main/java/de/csbd/learnathon/command/PunctaPickerView.java
@@ -759,7 +759,7 @@ public class PunctaPickerView {
 					model.processSemiAutomatedFlow();
 					ArrayList< FlowVector > autoFeatureFlow = model.getFlowVectorsCollection().getAutofeatureFlowVectors();
 					flowOverlay.setAutoFeatureFlow( autoFeatureFlow );
-				} else if ( string == "Optical Flow Fernback" ) {
+				} else if ( string == "Optical Flow Farneback" ) {
 					model.processOpticalFlowFernback(1, 0.5, showAutoFlowOnlyFlag, 5, 2, 5, 1.1, 0);
 				}
 
